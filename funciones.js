1037ccbf0dacbd1b535ab43e7b02ae2a27d08e38
Brahim006 <59586555+Brahim006@users.noreply.github.com>
2020-06-17 13:48:31 -0300

    Añadida la función Despedir en funciones.js

--- funciones.js
+++ funciones.js
@@ -1,4 +1,8 @@
-functio saludar(nombre) {
-    console.log('Hola '+nombre);
+function saludar(nombre) {
+    console.log('Hola ' + nombre);
     
+}
+
+function despedir(nombre){
+    console.log("Chau " + nombre);
 }
