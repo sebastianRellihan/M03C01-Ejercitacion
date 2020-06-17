a5821ccf3f5abdf2dfeb5e6f21cc91efa24f2158
Brahim006 <59586555+Brahim006@users.noreply.github.com>
2020-06-17 15:19:50 -0300

    encapsulado el comportamiento de las funciones dentro del objeto persona

--- funciones.js
+++ funciones.js
@@ -1,12 +1,9 @@
-function saludar(nombre) {
-<<<<<<< HEAD
-    console.log('Hola ' + nombre);
-    
-}
-
-function despedir(nombre){
-    console.log("Chau " + nombre);
-=======
-    console.log('Hola '+nombre);
->>>>>>> 237e68ab219adb4ed004ec5e996962b19a5c051c
+let persona = {
+    saludar : function (nombre) {
+        console.log('Hola ' + nombre);
+        
+    },
+    despedir : function (nombre){
+        console.log("Chau " + nombre);
+    }
 }
