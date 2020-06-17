4d1e753e58a93c25e9fd263f4a771134c4156c7a
sebastianRellihan <srellihan@gmail.com>
2020-06-17 14:50:40 -0300

    Agregue una funcion llamada mayor a funciones.js

--- funciones.js
+++ funciones.js
@@ -1,12 +1,16 @@
 function saludar(nombre) {
-<<<<<<< HEAD
     console.log('Hola ' + nombre);
     
 }
 
 function despedir(nombre){
     console.log("Chau " + nombre);
-=======
-    console.log('Hola '+nombre);
->>>>>>> 237e68ab219adb4ed004ec5e996962b19a5c051c
+}
+
+function mayor(edad) {
+    if(edad>=18) {
+        console.log('Sos mayor de edad');      
+    } else {
+        console.log('Sos menor de edad');
+    }
 }
