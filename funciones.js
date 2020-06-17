1f3698819d17444a16c74de629e507750647a2f2
Brahim006 <59586555+Brahim006@users.noreply.github.com>
2020-06-17 15:25:23 -0300

    estructuración del objeto persona, documentados sus métodos

--- funciones.js
+++ funciones.js
@@ -1,23 +1,28 @@
 let persona = {
+    /**
+     * Saluda a una persona por su nombre.
+     * @param {string} nombre El nombre de la persona a la que se desea saludar.
+     */
     saludar : function (nombre) {
         console.log('Hola ' + nombre);
         
     },
+    /**
+     * Despide a una persona por su nombre.
+     * @param {string} nombre El nombre de la persona a la que se desea despedir.
+     */
     despedir : function (nombre){
         console.log("Chau " + nombre);
-function saludar(nombre) {
-    console.log('Hola ' + nombre);
-    
-}
-
-function despedir(nombre){
-    console.log("Chau " + nombre);
-}
-
-function mayor(edad) {
-    if(edad>=18) {
-        console.log('Sos mayor de edad');      
-    } else {
-        console.log('Sos menor de edad');
+    },
+    /**
+     * Determina si la persona es mayor de edad.
+     * @param {number} edad La edad de la persona en cuestión.
+     */
+    esMayor : function(edad) {
+        if(edad>=18) {
+            console.log('Sos mayor de edad');      
+        } else {
+            console.log('Sos menor de edad');
+        }
     }
 }
